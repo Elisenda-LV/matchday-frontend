fd7664030869e2fd32830cb5d3a27c79c5416188
Elisenda-LV <elisenda.lv@gmail.com>
2024-02-02 12:59:10 +0100

    main-page

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { League } from '../../interfaces/league.interface';
+import { LeagueService } from '../../services/leagues.service';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
+
+
 export class MainPageComponent {
+
+  public leagues: League[] = [];
+
+  constructor( public leagueService: LeagueService ){
+    this.leagueService.getListLeagues().subscribe((response) => {this.leagues = response})
+
+  }
+
 
 }
